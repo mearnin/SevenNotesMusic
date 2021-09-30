aa9a2887986de04a22ed6924a7511cfad8a533a3
mearnin <87330597+mearnin@users.noreply.github.com>
2021-09-30 12:33:48 +0530

    Update play.py

--- sevennotes/plugins/play.py
+++ sevennotes/plugins/play.py
@@ -13,7 +13,7 @@ from youtube_dl import YoutubeDL
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pytgcalls import GroupCallFactory
-from sevennotes.userbot import User
+from sevennotes.plugins.userbot import User
 
 
 group_call = GroupCallFactory(User, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM).get_group_call()
