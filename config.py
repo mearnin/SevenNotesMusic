46c772adce724eb3fad4508401967344c86049f4
mearnin <87330597+mearnin@users.noreply.github.com>
2021-09-30 12:30:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+SESSION_NAME = getenv("SESSION_NAME", "")
