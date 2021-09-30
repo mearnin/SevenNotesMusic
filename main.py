8e43c72cbbe57d2448b488d542fbb3b1778994da
mearnin <87330597+mearnin@users.noreply.github.com>
2021-09-30 12:28:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from sevennotes.userbot import User
+from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
+
+Bot = Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="sevennotes.plugins"),
+)
+if not os.path.isdir("./downloads"):
+    os.makedirs("./downloads")
+
+Bot.start()
+User.start()
+print("\n[INFO] - STARTED VIDEO PLAYER BOT, JOIN @ASMSAFONE !")
+
+idle()
+Bot.stop()
+User.stop()
+print("\n[INFO] - STOPPED VIDEO PLAYER BOT, JOIN @ASMSAFONE !")
