c039265257a3c7fc8ea80844e5835516d486819e
sathaaaaaa <85862925+sathaaaaaa@users.noreply.github.com>
2021-09-30 14:59:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 SESSION_NAME = getenv("SESSION_NAME", "")
