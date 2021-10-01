248dee75d8dcbf2557f22337a1ce18ee09af1863
sathaaaaaa <85862925+sathaaaaaa@users.noreply.github.com>
2021-10-01 11:35:37 +0530

    Update __main__.py

--- sevennotes/__main__.py
+++ sevennotes/__main__.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from sevennotes.plugins.userbot import User
 from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
@@ -15,3 +16,10 @@ if not os.path.isdir("./downloads"):
 
 Bot.run()
 User.run()
+
+if __name__ == '__main__':
+    pyro_client = pyrogram.Client('pytgcalls', API_ID, API_HASH)
+    pyro_client.start()
+
+    asyncio.get_event_loop().run_until_complete(main(pyro_client))
+
